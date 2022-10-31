2e10e6697fe0ac33d3a60db06c9ec9411d7e2091
silas martins <silas07official@gmail.com>
2022-10-31 16:45:38 +0100

    Built the profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar";
-
+import Profile from "./components/Profile";
 function App() {
   return (
     <div className="mx-8 md:mx-10">
       <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import profile from "../assets/profileImage.png";
+
+const Profile = () => (
+  <section className="mt-2 md:mt-3 flex flex-row justify-center">
+    <div>
+      <img src={profile} alt="" id="profile__img" />
+      <h1 id="twitter" className="mt-1 text-[16px] md:text-[18px] font-poppins text-center">Silas Martins</h1>
+    </div>
+  </section>
+);
+
+export default Profile;
