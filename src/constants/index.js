aac51517e534abfe34fb972b3c80029f362782a1
silas martins <silas07official@gmail.com>
2022-10-31 17:10:00 +0100

    created a file to store the button links in array

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+export const btnLinks =[
+    {
+        id: "twitter__link",
+        goto: 'https://twitter.com/Silas07official',
+        label:'Twitter Link'
+    },
+    {
+        id: "btn__zuri",
+        goto: 'https://training.zuri.team',
+        label:'Zuri Team'
+    },
+    {
+        id: "books",
+        goto: 'https://books.zuri.team',
+        label:'Zuri Books'
+    },
+    {
+        id: "book__python",
+        goto: 'https://books.zuri.team/python-for-beginners?ref_id=yourslackname',
+        label:'Python Books'
+    },
+    {
+        id: "pitch",
+        goto: 'https://background.zuri.team',
+        label:'Background Checks for Coders'
+    },
+    {
+        id: "book__design",
+        goto: 'https://books.zuri.team/design-rules',
+        label:'Design Books'
+    },
+]
